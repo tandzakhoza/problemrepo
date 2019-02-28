673a07e10832e9700fa3a4081956ced755bc1b19
Tandza Khoza <G17K7453@ict.ru.ac.za>
2019-02-28 21:14:12 +0200

    fix bugs in program

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,41 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
+          public string Prefix(string input)
+          {
+           
+            int len=0;
+            foreach(char r in input)
+            {
+                len++;
+            }
+            if(len>0){
+            
+            string[] arr=input.Split(' ');
+            
+            int num=0;
+            foreach(string s in arr)
+            {
+              num++;
+            }
+
+
+           return len+","+num+":"+input;
+           
+           }
+           else
+           {
+            return "0,0";
+           }
+          
+          }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program p=new Program();
+            Console.WriteLine(p.Prefix("hello"));
         }
     }
 }
